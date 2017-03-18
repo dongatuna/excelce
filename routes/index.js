313919e791ce5cfb92fb836bcb5a137d8d537440
dongatuna <ngatuna05@gmail.com>
2017-03-18 15:52:28 -0700

    add get checkout path on index.js file.

--- routes/index.js
+++ routes/index.js
@@ -59,6 +59,16 @@ router.get('/shopping-cart', function (req, res, next) {
     res.render('pages/shopping-cart', {products:cart.generateArray(), totalPrice: cart.totalPrice});
 });
 
+router.get('/checkout', function (req, res, next) {
+    //if the user types gets checkout route without purchasing
+    if(!req.session.cart){
+        return res.redirect('pages/shopping-cart');
+    }
+
+    var cart = new Cart(req.session.cart);
+    res.render('shop/checkout', {total:cart.totalPrice});
+});
+
 module.exports = router;
 function notLoggedIn(req, res, next){
     if(!req.isAuthenticated()){
