d3d530768a96bc025af3c959e1eda70e0812d9cc
Charles Phillips <charles@doublerebel.com>
2017-03-26 16:34:53 -0700

    [deps] cookieParser is deprecated

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var expressHbs = require('express-handlebars');
 var mongoose = require('mongoose');
@@ -50,7 +49,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(validator());
-//app.use(cookieParser());
 app.use(session(
     {
         secret:'embabroswillmakeit',
